eff13c64fb807e4cfed1f8dfdd47c1b766769dbf
bastian-westholt <mail@bastianwestholt.com>
2025-12-09 21:05:25 +0100

    fetch blog posts from storage and generate blog homepage

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, request
+
+with open('data/storage.json', 'r') as reader:
+    blog_posts = json.loads(reader.read())
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return f'Hello World'
+def index():
+    return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        pass
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
